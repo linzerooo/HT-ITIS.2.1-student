1b8c0f1d2de8a7147dc5a036db0f3c4f112389e4
Liana <a>
2023-10-18 18:52:37 +0300

    hw2.1.6 написан калькулятор на Giraffe

---
+++ Program.fs
@@ -0,0 +1,22 @@
+﻿module Hw6.Hw6.Client.Program
+open System
+open System.Net.Http
+
+
+let sendRequestAsync (client: HttpClient) (url: string) =
+    async {
+        let! response = Async.AwaitTask (client.GetAsync url)
+        let! result = Async.AwaitTask (response.Content.ReadAsStringAsync())
+        return result
+    }
+    
+[<EntryPoint>]
+let main args =
+    use handler = new HttpClientHandler()
+    use client = new HttpClient(handler)
+    let input = Console.ReadLine()
+    let args = input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    if args.Length = 3 then
+        let url = $"http://localhost:58592/calculate?value1={args[0]}&operation={parseOperation args[1]}&value2={args[2]}";
+        printfn $"{Async.RunSynchronously(sendRequestAsync client url)}"
+    0
